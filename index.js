530085317b2bedd25631368f80e46fb431b39111
Willian R. Sabião <willianjedai@gmail.com>
2014-10-08 22:22:20 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = (function () {
+  'use Strict';
+
+  var arguments = process.argv;
+  var serializedArguments = {};
+
+  for (var i = 0; i <= arguments.length-1; i++) {
+    var arg = arguments[i];
+    if ( arg.substr(0,1) === '-' ) {
+      serializedArguments[arg.replace(/-/g, '')] = (arguments[i+1].substr(0,1) === '-') ? true : arguments[++i];
+    } else if (arg.indexOf('=') !== -1) {
+      arg = arg.match(/([^=]*)=(.*)/);
+      serializedArguments[arg[1]] = arg[2];
+    }
+  }
+
+  return serializedArguments;
+});
